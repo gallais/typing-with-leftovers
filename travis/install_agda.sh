b0289b34fc47afc67ef8d2881c63a7ef0748a989
G. Allais <guillaume.allais@ens-lyon.org>
2019-01-11 21:45:03 +0100

    [ travis ] reviving the build (#1)

--- travis/install_agda.sh
+++ travis/install_agda.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-if ! type "agda" > /dev/null || [ ! `agda -V | sed "s/[^2]*//"` = "2.5.1" ]; then
+if ! type "agda" > /dev/null || [ ! `agda -V | sed "s/[^2]*//"` = "2.5.3" ]; then
   cabal update
   cabal install alex happy cpphs
-  cabal install Agda
+  cabal install Agda-2.5.3
   mkdir -p $HOME/.agda
   cp libraries-2.5.3 $HOME/.agda/
   cd $HOME/.agda/
